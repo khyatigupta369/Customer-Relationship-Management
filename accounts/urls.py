2b641e90cad91a566760aeb9e44680658384d772
khyatisnbri <khyati@sbnri.com>
2023-09-10 13:54:46 +0530

    templates and template inheritance added

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,10 @@ from django.urls import path, include
 
 from . import views
 
+# url patters 'customer/' is correct and '/customer' is wrong
+# link name followed by slash
 urlpatterns = [
     path('', views.home),
-    path('/customer', views.customer),
-    path('/products', views.products),
+    path('customer/', views.customer),
+    path('products/', views.products),
 ]
